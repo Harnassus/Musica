b695d2eb5f678ea9f31af115b88a27f5426f172d
Michael Festus <micfestus45@gmail.com>
2022-10-26 04:02:06 +0100

    Gotten all the necessary infos from the endpoints, deleted services folder(no longer needed)

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,20 +2,25 @@ import {SECRET_CLIENT_ID, SECRET_CLIENT_SEC} from '$env/static/private'
 /** @type {import('./$types').PageServerLoad} */
 export const prerender = true;
 export const load = async () => {
-    const tokenEndpoint = 'https://accounts.spotify.com/api/token?grant_type=client_credentials';
-    const playlistEndpoint = 'https://api.spotify.com/v1/browse/featured-playlists'
 
+    // Endpoints
+    const tokenEndpoint = 'https://accounts.spotify.com/api/token?grant_type=client_credentials';
+    const playlistEndpoint = 'https://api.spotify.com/v1/browse/featured-playlists';
+    const newReleasesEndpoint = 'https://api.spotify.com/v1/playlists/37i9dQZF1DXbpmT3HUTsZm';
+    const popularTracksEndpoint = 'https://api.spotify.com/v1/playlists/75GeX247deNdTX7zdyZDnU';
+
+    // Requests
     const getToken = async () => {
-       const tokenRequest = await fetch(tokenEndpoint, {
+        const tokenRequest = await fetch(tokenEndpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 Authorization: 'Basic ' + btoa(SECRET_CLIENT_ID + ':' + SECRET_CLIENT_SEC)
             },
-       })
+        });
         const res = await tokenRequest.json();
         return res.access_token;
-    }
+    };
     
     const getPlaylists = async () => {
         const playlistsRequest = await fetch(playlistEndpoint, {
@@ -23,15 +28,36 @@ export const load = async () => {
                 Authorization: 'Bearer ' + await getToken()
             },
             method: 'GET'
-        })
+        });
         const res = await playlistsRequest.json();
-        return res.playlists.items
-    }
+        return res.playlists.items;
+    };
 
+    const getNewReleases = async () => {
+        const newReleasesRequest = await fetch(newReleasesEndpoint, {
+            headers: {
+                Authorization: 'Bearer ' + await getToken()
+            },
+            method: 'GET'
+        });
+        const res = await newReleasesRequest.json();
+        return res.tracks.items;
+    };
+
+    const getPopularTracks = async () => {
+        const popularTracksRequests = await fetch(popularTracksEndpoint, {
+            headers: { Authorization: 'Bearer ' + await getToken() },
+            method: 'GET'
+        });
+        const res = await popularTracksRequests.json();
+        return res.tracks.items;
+    }
     
     
     return {
         token: getToken(),
         playlists: getPlaylists(),
+        newReleases: getNewReleases(),
+        popularTracks: getPopularTracks(),
     }
 }
--- src/services/credentials.js
+++
@@ -1,8 +0,0 @@
-import { PUBLIC_CLIENT_ID, PUBLIC_CLIENT_SEC } from "$env/static/public";
-const Credentials = () => {
-    return {
-        clientId: PUBLIC_CLIENT_ID,
-        clientSecret: PUBLIC_CLIENT_SEC
-    }
-}
-export { Credentials };
