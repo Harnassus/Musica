3c8663dae1dd19de679d34c9a2bcc4bf6642f932
Michael Festus <micfestus45@gmail.com>
2022-10-26 02:18:58 +0100

    defining endpoints

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,23 +2,36 @@ import {SECRET_CLIENT_ID, SECRET_CLIENT_SEC} from '$env/static/private'
 /** @type {import('./$types').PageServerLoad} */
 export const prerender = true;
 export const load = async () => {
-    const tokenUrl = 'https://accounts.spotify.com/api/token?grant_type=client_credentials';
-    const playlistEndpoint = ''
+    const tokenEndpoint = 'https://accounts.spotify.com/api/token?grant_type=client_credentials';
+    const playlistEndpoint = 'https://api.spotify.com/v1/browse/featured-playlists'
 
-    const tokenRequest = await fetch(tokenUrl, {
-        method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				Authorization: 'Basic ' + btoa(SECRET_CLIENT_ID + ':' + SECRET_CLIENT_SEC)
-			},
-			
-    })
-    const access = await tokenRequest.json();
-    const accessToken = access.access_token;
-
-    const getPlaylists = await fetch()
-    return {
-        token: access,
+    const getToken = async () => {
+       const tokenRequest = await fetch(tokenEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: 'Basic ' + btoa(SECRET_CLIENT_ID + ':' + SECRET_CLIENT_SEC)
+            },
+       })
+        const res = await tokenRequest.json();
+        return res.access_token;
+    }
     
+    const getPlaylists = async () => {
+        const playlistsRequest = await fetch(playlistEndpoint, {
+            headers: {
+                Authorization: 'Bearer ' + await getToken()
+            },
+            method: 'GET'
+        })
+        const res = await playlistsRequest.json();
+        return res.playlists.items
+    }
+
+    
+    
+    return {
+        token: getToken(),
+        playlists: getPlaylists(),
     }
 }
